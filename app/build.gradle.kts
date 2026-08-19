55088a42c790dfcf9a6411de0b92f223088d5ec3
andrewtate1105-ship-it <andrewtate1105@gmail.com>
2026-08-19 10:28:52 +0530

    Version TradingView signal build as 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.institutionaltrading.mobile"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0-cleanroom"
+        versionCode = 3
+        versionName = "0.3.0-tradingview"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "MARKET_DATA_BASE_URL", "\"$marketDataBaseUrlLiteral\"")
     }
