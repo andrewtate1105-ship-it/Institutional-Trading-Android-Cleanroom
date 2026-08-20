9fb9a66de0fb94abf22cdaaf217690d7c6312c5f
andrewtate1105-ship-it <andrewtate1105@gmail.com>
2026-08-20 18:48:07 +0530

    Version institutional signal app v0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.institutionaltrading.mobile"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.3.0-tradingview"
+        versionCode = 4
+        versionName = "0.4.0-institutional"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "MARKET_DATA_BASE_URL", "\"$marketDataBaseUrlLiteral\"")
     }
